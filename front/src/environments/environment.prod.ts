25caf7343ec7541ae279b7efafd3fe6f0a4f7a2c
Jose Hernandez <jose@djobnet.com>
2026-05-20 17:41:05 -0400

    change in api domain

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://147.93.3.184:3010/api/v1',
+  apiUrl: 'https://markconverted.zapto.org/api/v1',
   primaryColor: '#0056b3',
   secondaryColor: '#00d1b2',
 };
